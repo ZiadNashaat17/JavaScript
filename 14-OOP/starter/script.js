7b300b9a5027390b0130b669cae29505eca9ed67
ZiadNashaat17 <Ziadnashaat03@gmail.com>
2024-11-23 15:35:55 +0200

    New Lesson

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,2 +1,26 @@
 'use strict';
-//
+
+const Person = function (firstName, birthYear) {
+  // Instance Properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never do this
+  // this.calcAge = function () {
+  //   return 2024 - this.birthYear;
+  // };
+};
+
+const ziad = new Person('Ziad', 2003);
+console.log(ziad);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const matilda = new Person('Matilda', 2005);
+const jack = new Person('Jack', 2010);
+console.log(matilda, jack);
+
+console.log(ziad instanceof Person);
